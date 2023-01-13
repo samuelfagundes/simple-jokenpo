b398518e4e5e315b78661456b4a74ce972ea0eec
Samuel Fagundes <samuelfagundessva@gmail.com>
2023-01-12 21:19:56 -0300

    add active buttons style

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ export function App() {
   const [cpuChoice, setCPUChoice] = useState("");
   const [winner, setWinner] = useState("");
   const [isResultHidden, setIsResultHidden] = useState(true);
+  const [isActive, setIsActive] = useState("");
 
   function handleChoice(chosenOne: string) {
-    setChoice(chosenOne)
-    setIsResultHidden(true)
-    computerChoice()
+    setChoice(chosenOne);
+    setIsActive(chosenOne)
+    setIsResultHidden(true);
+    computerChoice();
   }
 
   function computerChoice() {
@@ -26,27 +28,28 @@ export function App() {
   }
 
   function getWinner() {
-    setIsResultHidden(false)
+    setIsResultHidden(false);
     if (
       (choice === "Rock" && cpuChoice === "Scissors") ||
       (choice === "Paper" && cpuChoice === "Rock") ||
       (choice === "Scissors" && cpuChoice === "Paper")
     ) {
-      setWinner('Player')
+      setWinner("Player");
     } else if (
       (choice === "Rock" && cpuChoice === "Paper") ||
       (choice === "Paper" && cpuChoice === "Scissors") ||
       (choice === "Scissors" && cpuChoice === "Rock")
     ) {
-      setWinner('Computer')
+      setWinner("Computer");
     } else if (
       (choice === "Rock" && cpuChoice === "Rock") ||
       (choice === "Paper" && cpuChoice === "Paper") ||
       (choice === "Scissors" && cpuChoice === "Scissors")
     ) {
-      setWinner('Draw')
+      setWinner("Draw");
     }
-    setChoice('')
+    setChoice("");
+    setIsActive('')
   }
 
   return (
@@ -56,16 +59,25 @@ export function App() {
       </h1>
       <section className="flex-1">
         <span className="font-bold">Choose wisely:</span>
-        <Button handleChoice={() => handleChoice("Rock")}>Rock</Button>
-        <Button handleChoice={() => handleChoice("Paper")}>Paper</Button>
-        <Button handleChoice={() => handleChoice("Scissors")}>Scissors</Button>
+        <Button handleChoice={() => handleChoice("Rock")} isActive={isActive === "Rock"}>Rock</Button>
+        <Button handleChoice={() => handleChoice("Paper")} isActive={isActive === "Paper"}>Paper</Button>
+        <Button handleChoice={() => handleChoice("Scissors")} isActive={isActive === "Scissors"}>Scissors</Button>
       </section>
       <section className="mb-8">
-        <span>{isResultHidden ? "Your opponent is choosing..." : ('Computer chose: ' + cpuChoice)}</span>
+        <span>
+          {isResultHidden
+            ? "Your opponent is choosing..."
+            : "Computer chose: " + cpuChoice}
+        </span>
       </section>
-      <button className="bg-[#427AA1] m-2 w-24 text-white rounded hover:bg-blue-700 transition ease-in-out duration-300" onClick={() => getWinner()}>Go!</button>
+      <button
+        className="bg-[#427AA1] m-2 mb-8 w-24 text-white rounded hover:bg-blue-700 transition ease-in-out duration-300"
+        onClick={() => getWinner()}
+      >
+        Go!
+      </button>
       <section>
-        <span className="text-xl">Result: {winner}</span>
+        <span className="text-xl">Result: {winner === 'Player' ? 'You win! :D' : winner === 'Computer' ? 'You lose! :(' : winner === "Draw" ? "Draw ._." : ''}</span>
       </section>
     </div>
   );
--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,12 +1,29 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode
-  handleChoice: () => void
+  children: ReactNode;
+  handleChoice: () => void;
+  isActive: boolean
 }
 
-export function Button({ children, handleChoice }: ButtonProps) {
+export function Button({ children, handleChoice, isActive }: ButtonProps) {
   return (
-    <button className="bg-[#427AA1] m-2 w-24 text-white rounded hover:bg-blue-700 transition ease-in-out duration-300" onClick={() => handleChoice()}>{children}</button>
-  )
+    <button
+      className={
+        `bg-[#6097BE]
+          m-4
+          w-24
+          text-white
+          rounded
+          hover:bg-blue-700
+          transition
+          ease-in-out
+          duration-300
+          ${isActive ? "bg-[#112d40]" : ''}`
+      }
+      onClick={() => handleChoice()}
+    >
+      {children}
+    </button>
+  );
 }
